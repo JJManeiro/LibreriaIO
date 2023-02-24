7b3f3766a93068294f132b7484c899be42d4c0e4
Juan José Maneiro <jmaneirogarcia@danielcastelao.org>
2023-02-24 09:45:23 +0100

    añadido de javadoc e imágenes del intento anterior

---
+++ LibreriaIO/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"raiburari","l":"LibreriaIO"},{"p":"raiburari","l":"Main"},{"p":"raiburari","l":"Operation"}];updateSearchResults();
