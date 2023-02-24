7b50da33e27c93ba352e6d0f0c626111cc912ae9
Juan José Maneiro <jmaneirogarcia@danielcastelao.org>
2023-02-24 09:25:21 +0100

    nueva version

--- LibreriaIO/src/raiburari/LibreriaIO.java
+++ LibreriaIO/src/raiburari/LibreriaIO.java
@@ -3,8 +3,19 @@ package raiburari;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 public class LibreriaIO {
+    /**
+     * Esta es la librería calculadora.Ambos consola y popup tendrán constantes finales.
+     * @param Consola llamará un scanner para pedir los numeros y nombre.
+     * @param Popup usará JOptionPane para pedir los numeros y nombre.
+     */
     public static final int Consola = 1;
     public static final int Popup = 2;
+
+    /**
+     * Este será el switch que activará la salida,en el están
+     * @param m que dará el resultado que pondremos a leer en el scanner/JoptionPane.
+     * @param op será lo que pondremos en el switch para escoger la opción de consola o popup.
+     */
     public static void Output(String m,int op){
         switch (op){
             case Consola:
@@ -15,6 +26,13 @@ public class LibreriaIO {
                 break;
         }
     }
+
+    /**
+     * Con la misma mećanica activamos la entrada
+     * @param com dará el resultado que pondremos a leer en el scanner/JoptionPane.
+     * @param op ayudara a escoger en el switch.
+     * @return será null.
+     */
     public static String SInput (String com,int op){
         switch (op){
             case Consola:
@@ -26,6 +44,14 @@ public class LibreriaIO {
         }
         return null;
     }
+
+    /**
+     * Casi lo mismo aquí pero con un extra.
+     * @param com dará el resultado que pondremos a leer en el scanner/JoptionPane(El JOP será casteado a Integer).
+     * @param op ayudara a escoger en el switch.
+     * @return null.
+     * @throws Exception en el caso que dejemos el numero en blanco.
+     */
     public static Integer NInput (String com,int op) throws Exception{
         try {
             switch (op) {
@@ -42,6 +68,14 @@ public class LibreriaIO {
         }
         return null;
     }
+
+    /**
+     * Misma mecánica que con los números.
+     * @param com dará el resultado que pondremos a leer en el scanner/JoptionPane(El JOP será casteado a Float).
+     * @param op ayudara a escoger en el switch.
+     * @return null.
+     * @throws Exception en el caso que dejemos el numero en blanco.
+     */
     public static Float FInput (String com,int op) throws Exception{
         try {
             switch (op) {
