503139e59908afd14c2d2d5c8a64c5d2b632d301
Juan José Maneiro <jmaneirogarcia@danielcastelao.org>
2023-02-24 09:31:12 +0100

    intento de squash 1

--- LibreriaIO/src/raiburari/Main.java
+++ LibreriaIO/src/raiburari/Main.java
@@ -1,9 +1,29 @@
 package raiburari;
 
+/**
+ * Libreria de calculos + Libreria de Inputs/Outputs.
+ * @author Juan
+ * @version 24.2.2023
+ */
 public class Main {
     public static void main(String[] args) throws Exception {
+        /**
+         * Llamamos a los objetos de la librería IO y la calculadora
+         */
         LibreriaIO L = new LibreriaIO();
         Operation O = new Operation();
+        /**
+         * Librería IO
+         * @param name pide un nombre en forma de String.
+         * @return nombre vacio si se deja en blanco.
+         * @return el nombre que hayamos puesto si no está vació.
+         * @param N pide un numero Integer.
+         * @return dame numero si se deja vacío.
+         * @return el numero dado si pusimos uno.
+         * @param F pide un numero float.
+         * @return dame numero si se deja vacío.
+         * @return el numero dado si pusimos uno.
+         */
         String name = LibreriaIO.SInput("nombre?",L.Popup);
         if (name.isEmpty()==true){
             LibreriaIO.Output("nombre en vacío",L.Popup);
@@ -25,6 +45,9 @@ public class Main {
         else{
             LibreriaIO.Output(F.toString()+"\nFue ese tu numero?",L.Popup);
         }
+        /**
+         * Llama a la librería calculadora. Esta se verá en las otras clases.
+         */
         O.Calc();
     }
 }
--- LibreriaIO/src/raiburari/Operation.java
+++ LibreriaIO/src/raiburari/Operation.java
@@ -1,8 +1,22 @@
 package raiburari;
 
 public class Operation {
-    public static final String suma="S",resta ="R",multi="M",divi="D";
+    /**
+     * Esta es la librería calculadora.
+     * Usaremos como operadores las letras S,R,M,D y V, las cuales son constantes finales de String.
+     * @param float será el numero nulo que cambiará al resultado de la operación cuando toque.
+     */
+    public static final String suma="S",resta ="R",multi="M",divi="D",raiz="V";
     public static Float total=null;
+
+    /**
+     * Este es el switch que usaremos para decir que operaciones usaremos en la calculadora.
+     * @param n1 será el primero numero que pondremos, el dividendo a la hora de dividir y el radicando a la hora de hacer raíces.
+     * @param n2 será el otro numero a poner. El divisor al dividir y el índice al hacer raíces.
+     * @param O será la letra que pondremos para saber que case queremos en el switch.
+     * @return total que dará el resultado de la operacíon que hicimos.
+     * @throws Exception en el caso de que pongamos un numero 0 a la hora de dividir o hacer ráices.
+     */
     public float Operation(float n1,float n2,String O)throws Exception{
         switch (O){
             case suma:
@@ -20,17 +34,34 @@ public class Operation {
                 }
                 total=n1/n2;
                 break;
+            case raiz:
+                if (n1==0||n2==0){
+                    throw new Exception("Que complicado no?");
+                }
+                total = (float)Math.pow(n1,(1/n2));
         }
         return total;
     }
+
+    /**
+     * Esto será el comprobante que usaremos en el caso de poner letras que no hemos marcado como operadores.
+     * @param O será la letra que pondremos a la hora de operar.
+     * @throws Exception si no hemos puesto uno de las letras operadoras ahí.
+     */
     public static void Validate (String O) throws Exception{
-        if(!O.equals("S")&&!O.equals("R")&&!O.equals("M")&&!O.equals("D")){
+        if(!O.equals("S")&&!O.equals("R")&&!O.equals("M")&&!O.equals("D")&&!O.equals("V")){
             throw new Exception("Parámetro Invalido");
         }
     }
+
+    /**
+     * Calc será el que cogerá el Input de los floats n1 y n2, tras haberse validado el String O.
+     * R será el resultado que nos dará tras operar n1 y n2. Este se mostrará en el Output
+     * Pilla las excepciones y los mensajes que hemos tirado en validate y operation de esta librería.
+     */
     public void Calc(){
         try {
-            String O = LibreriaIO.SInput("S suma\nR resta\nM multiplica\nD divide", LibreriaIO.Popup);
+            String O = LibreriaIO.SInput("S suma\nR resta\nM multiplica\nD divide \nV raices", LibreriaIO.Popup);
             Validate(O);
             float n1 = LibreriaIO.FInput("numero 1?", LibreriaIO.Popup);
             float n2 = LibreriaIO.FInput("numero 2?", LibreriaIO.Popup);
